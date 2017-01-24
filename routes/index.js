db3b5113e9db71f977ccea819ea2bc6a4baa983f
joshpaulchan <joshpaulchan@gmail.com>
2017-01-24 00:16:47 -0500

    Consolidate route files

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// GET `/autenticate`
+// Bounce the plaid public token back to plaid and save the access token
+// 
+// 
+router.get('/authenticate', (req, res) => {
+  // save the public token
 });
 
 module.exports = router;
