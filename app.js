5dbe46ed605c0b29857b8489b5f67f651e83a2c0
joshpaulchan <joshpaulchan@gmail.com>
2017-01-29 00:57:14 -0500

    Add config and plaid client

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+require('dotenv').config();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,9 +8,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var plaid = require('plaid');
 
 var app = express();
+
+// connect and configure plaid
+var plaidClient = new plaid.Client(
+  process.env.PLAID_CLIENT_ID,
+  process.env.PLAID_SECRET,
+  plaid.environments.tartan
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +31,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+//////////////////////////////////// ROUTES ////////////////////////////////////
+
+app.use('/', require('./routes/index'));
+app.use('/', require('./routes/plaid')(plaidClient));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
